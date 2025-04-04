fad831c33fa6f6b79e56d849d22097ddb19701b9
Tomshr29 <tomscherer29@gmail.com>
2025-04-04 22:12:03 +0200

    install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
     react(),
     adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
