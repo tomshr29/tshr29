c9f639b87ee15830095ec945e97edcdd938251d4
Tomshr29 <tomscherer29@gmail.com>
2025-04-05 22:22:47 +0200

    Update home

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,13 +1,23 @@
-import { Head } from '@inertiajs/react'
-import Header from '~/app/components/header'
+import { Link } from '@inertiajs/react'
 
 export default function Home() {
   return (
-    <>
-      <Head title="Tom Scherer" />
-
-      <Header />
-      <h1 className="text-3xl font-bold underline">This is HomePage</h1>
-    </>
+    <div className="grid place-items-center items-center h-screen">
+      <div className="mx-auto text-center max-w-xl">
+        <h1 className="text-7xl font-medium antialiased scale-y-95 tracking-tighter">T29 Tech</h1>
+        <p className="mt-4 text-pretty text-lg tracking-tight scale-y-95">
+          Your one-stop solution for all your tech needs. We provide a wide range of services,
+          including web development, mobile app development, and more.
+        </p>
+        <div className="mt-10 flex justify-center gap-6">
+          <Link
+            href="#"
+            className="text-lg font-semibold py-3 px-4 rounded-2xl antialiased bg-black text-white"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
