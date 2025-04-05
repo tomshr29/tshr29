9adbd18b4cab15c78d51c6e1b1c5180de5f36c2c
Tomshr29 <tomscherer29@gmail.com>
2025-04-05 21:19:33 +0200

    Ajout du header

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,10 +1,12 @@
 import { Head } from '@inertiajs/react'
+import Header from '~/app/components/header'
 
 export default function Home() {
   return (
     <>
       <Head title="Tom Scherer" />
 
+      <Header />
       <h1 className="text-3xl font-bold underline">This is HomePage</h1>
     </>
   )
