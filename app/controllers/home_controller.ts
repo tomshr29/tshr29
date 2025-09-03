865ef498435ab43d81f103e5391a7a4ef9a6fd23
Tomshr29 <tomscherer29@gmail.com>
2025-09-03 21:06:28 +0200

    manifest error

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -2,6 +2,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HomeController {
   index({ inertia }: HttpContext) {
-    return inertia.render('home')
+    return inertia.render('index')
   }
 }
